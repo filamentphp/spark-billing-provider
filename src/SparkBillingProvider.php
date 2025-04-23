c2515116a8a20770f8fb9b419850a54d77d1a6c0
Dan <git@danharrin.com>
2025-04-23 06:39:45 +0000

    Merge pull request #16096 from yandearta/3.x

--- src/SparkBillingProvider.php
+++ src/SparkBillingProvider.php
@@ -9,7 +9,7 @@ use Filament\Facades\Filament;
 use Illuminate\Http\RedirectResponse;
 use Spark\Spark;
 
-class SparkBillingProvider implements Contracts\BillingProvider
+class SparkBillingProvider implements Contracts\Provider
 {
     /**
      * @return string | Closure | array<class-string, string>
